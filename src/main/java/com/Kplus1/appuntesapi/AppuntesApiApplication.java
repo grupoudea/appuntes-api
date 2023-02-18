b9becbca3dfaa1095da3dd8fbe7d778ec8ef414a
kellytbn <kellytbn14@gmail.com>
2023-02-18 13:05:58 -0500

    Instalacion proyecto

---
+++ src/main/java/com/Kplus1/appuntesapi/AppuntesApiApplication.java
@@ -0,0 +1,13 @@
+package com.Kplus1.appuntesapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppuntesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppuntesApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Kplus1/appuntesapi/AppuntesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Kplus1.appuntesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppuntesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
