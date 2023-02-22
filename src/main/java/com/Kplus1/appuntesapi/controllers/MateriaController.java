329a50d2eab140bd9b2c04fe5057a98545b8a62e
kellytbn <kellytbn14@gmail.com>
2023-02-21 21:34:36 -0500

    Servicio para crear materias

--- src/main/java/com/Kplus1/appuntesapi/controllers/MateriaController.java
+++ src/main/java/com/Kplus1/appuntesapi/controllers/MateriaController.java
@@ -1,6 +1,11 @@
 package com.Kplus1.appuntesapi.controllers;
 
+import com.Kplus1.appuntesapi.dtos.MateriaDto;
 import com.Kplus1.appuntesapi.services.MateriaService;
+import com.Kplus1.appuntesapi.utils.StandardResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,5 +17,10 @@ public class MateriaController {
 
     public MateriaController(MateriaService materiaService) {
         this.materiaService = materiaService;
+    }
+
+    @PostMapping("/guardar-materia")
+    public ResponseEntity<StandardResponse<MateriaDto>> saveGrades(@RequestBody MateriaDto materia) {
+        return ResponseEntity.ok(new StandardResponse<>(materiaService.crearMateria(materia)));
     }
 }
--- src/main/java/com/Kplus1/appuntesapi/services/MateriaService.java
+++ src/main/java/com/Kplus1/appuntesapi/services/MateriaService.java
@@ -1,10 +1,13 @@
 package com.Kplus1.appuntesapi.services;
 
+import com.Kplus1.appuntesapi.dtos.MateriaDto;
 import com.Kplus1.appuntesapi.mappers.MateriaMapper;
 import com.Kplus1.appuntesapi.repositories.MateriaRepository;
+import org.hibernate.ObjectNotFoundException;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.Objects;
 
 @Service
 @Transactional
@@ -16,5 +19,12 @@ public class MateriaService {
     public MateriaService(MateriaRepository materiaRepository, MateriaMapper materiaMapper) {
         this.materiaRepository = materiaRepository;
         this.materiaMapper = materiaMapper;
+    }
+
+    public MateriaDto crearMateria(MateriaDto materia) {
+        if (Objects.isNull(materia) || Objects.isNull(materia.getIdMateriaFk()) || Objects.isNull(materia.getIdEstudianteFk())) {
+            throw new ObjectNotFoundException(null, "Faltan campos requeridos");
+        }
+        return materiaMapper.toDto(materiaRepository.save(materiaMapper.toEntity(materia)));
     }
 }
